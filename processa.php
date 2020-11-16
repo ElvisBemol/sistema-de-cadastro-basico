bcbd738beca3a6fcf954c0bb68f94bf0764ab4bd
ElvisBemol <elviscarlosdeo@gmail.com>
2020-11-16 17:37:04 -0300

    pagina de cadastro -processa ok

--- processa.php
+++ processa.php
@@ -1,4 +1,12 @@
 <?php
 include_once("conexao.php");
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$profissao = $_POST['profissao'];
+
+$sql ="insert into usuarios (nome, email,profissao) values ('$nome','$email','$profissao')"; // variável que vai armazenar comandos sql
+$salvar = mysqli_query($connexao,$sql); //variável que vai guardar comandos que vão registrar  
+
 mysqli_close($connexao);
 ?>
