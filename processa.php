26a96076ef115049ae07d67c10b821a82b108e52
ElvisBemol <elviscarlosdeo@gmail.com>
2020-11-16 17:56:33 -0300

    Validação de cadastro
    informa o motivo de não cadastrar usuários com E-mail repetido

--- processa.php
+++ processa.php
@@ -8,5 +8,54 @@ $profissao = $_POST['profissao'];
 $sql ="insert into usuarios (nome, email,profissao) values ('$nome','$email','$profissao')"; // variável que vai armazenar comandos sql
 $salvar = mysqli_query($connexao,$sql); //variável que vai guardar comandos que vão registrar  
 
+
+$linhas = mysqli_affected_rows($connexao);// verificar linhas afetadas 
+
+
 mysqli_close($connexao);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Cadastro</title>
+    <link rel="stylesheet" href="_css/estylo.css">
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+            <ul class="menu">
+                <a href="index.html">
+                    <li>Cadastro</li>
+                </a>
+
+                <a href="consultas.php">
+                    <li>Consultas</li>
+                </a>
+            </ul>
+        </nav>
+        <section>
+            <h1>Confirmação de Cadastro</h1>
+            <hr><br><br>
+
+            <?php
+
+            if ($linhas == 1){
+                print "Cadastro efetuado com sucesso";
+            }else{
+                print "Cadastro não efetuado. <br> Já existe um usuário com esse E-mail";
+            }
+
+            ?>
+            
+        </section>
+    </div>
+</body>
+
+</html>
+<!--proxima aula https://www.youtube.com/watch?v=ejnU-17yd8A-->
